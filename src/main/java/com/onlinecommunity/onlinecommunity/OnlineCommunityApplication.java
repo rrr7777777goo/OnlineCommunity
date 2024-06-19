3637dacc9763fa6a90057b60d49de7c4d83ae658
rrr7777777goo <rrr7777777goo@gmail.com>
2024-06-19 18:47:38 +0900

    Initial Commit

---
+++ src/main/java/com/onlinecommunity/onlinecommunity/OnlineCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.onlinecommunity.onlinecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinecommunity/onlinecommunity/OnlineCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinecommunity.onlinecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
